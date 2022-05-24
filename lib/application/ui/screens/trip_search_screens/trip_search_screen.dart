91b72a448fa8fd7df0882edd313869fd96a819fd
Poll <88119338+Poll21@users.noreply.github.com>
2022-05-24 08:27:59 +0700

    cf

--- lib/application/ui/screens/trip_search_screens/trip_search_screen.dart
+++ lib/application/ui/screens/trip_search_screens/trip_search_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:untitled2/application/ui/constants/constants.dart';
@@ -15,187 +17,218 @@ class _TripSearchScreenState extends State<TripSearchScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(child:
-      Padding(
-        padding: const EdgeInsets.only(right: 25, left: 25,top: 50),
-        child: Column(
-          children: [
-
-            Image.asset('assets/images/trip_search.png', height: 176,),
-            SizedBox(
-              height: 7,
-            ),
-            TripSearchCard(),
-            SizedBox(
-              height: 36,
-            ),
-            Image.asset('assets/images/search_location.png',height: 100,),
-            const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 25, vertical: 20),
-              child: Text('Здесь будет отображаться список ваших поисков',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontFamily: 'Montserrat',
-                    fontWeight: FontWeight.w700,
-                    fontSize: 14,
-                    color: textActiveColor,
-                  )),
-            ),
-
-          ],
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.only(right: 25, left: 25, top: 50),
+          child: Column(
+            children: [
+              Image.asset(
+                'assets/images/trip_search.png',
+                height: 176,
+              ),
+              const SizedBox(
+                height: 7,
+              ),
+              const TripSearchCard(),
+              const SizedBox(
+                height: 36,
+              ),
+              Image.asset(
+                'assets/images/search_location.png',
+                height: 100,
+              ),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 25, vertical: 20),
+                child: Text('Здесь будет отображаться список ваших поисков',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontFamily: 'Montserrat',
+                      fontWeight: FontWeight.w700,
+                      fontSize: 14,
+                      color: textActiveColor,
+                    )),
+              ),
+            ],
+          ),
         ),
-      ),),
-
+      ),
     );
   }
 }
 
 class TripSearchCard extends StatelessWidget {
-   const TripSearchCard({Key? key,
-     }) : super(key: key);
+  const TripSearchCard({
+    Key? key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-
     return SizedBox(
-      height: 300,
+      height: 310,
       width: 340,
       child: Card(
         elevation: 0,
         color: backGroundColor,
-        shape:
-        const RoundedRectangleBorder(
+        shape: const RoundedRectangleBorder(
             side: BorderSide(color: Color(0xFFE0E0E0), width: 1),
-            borderRadius: BorderRadius.all(Radius.circular(10))),
-        child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Expanded(
-                child: Column(
-                  children: [
-                    Padding(
-                        padding: EdgeInsets.only(
-                          right: 27, left: 27,
-                          top: 17),
-                      child: TextField(
-                        cursorColor: textPassiveColor,
-                        style: TextStyle(color: textPassiveColor,
+            borderRadius: BorderRadius.all(Radius.circular(15))),
+        child: Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
+          Expanded(
+            child: Column(
+              children: [
+                Padding(
+                    padding: const EdgeInsets.only(right: 27, left: 27, top: 17),
+                    child: TextField(
+                      cursorColor: textPassiveColor,
+                      style: const TextStyle(
+                        color: textPassiveColor,
+                        fontSize: 14,
+                        fontFamily: 'Montserrat',
+                        fontWeight: FontWeight.w600,
+                      ),
+                      decoration: InputDecoration(
+                          isCollapsed: true,
+                          contentPadding:
+                              const EdgeInsets.symmetric(horizontal: 0, vertical: 9),
+                          hintText: 'Откуда',
+                          hintStyle: const TextStyle(
+                            color: textPassiveColor,
                             fontSize: 14,
-                            fontFamily:'Montserrat',
-                            fontWeight: FontWeight.w600,),
-                        decoration: InputDecoration(
-                         prefix: Icon(
-                           UiIcons.vector_1,
-                           color: textPassiveColor,
-                           size: 17,),
-                         isCollapsed: true,
-                         contentPadding: EdgeInsets.symmetric(horizontal: 0, vertical: 9),
-                         hintText: 'Откуда',
-                         hintStyle: TextStyle(color: textPassiveColor,
-                            fontSize: 14,
-                            fontFamily:'Montserrat',
+                            fontFamily: 'Montserrat',
                             fontWeight: FontWeight.w600,
                           ),
-                          enabledBorder: UnderlineInputBorder(
+                          enabledBorder: const UnderlineInputBorder(
                             borderSide: BorderSide(
                               color: textPassiveColor,
                             ),
                           ),
-                          focusedBorder: UnderlineInputBorder(
+                          focusedBorder: const UnderlineInputBorder(
                             borderSide: BorderSide(
                               color: textPassiveColor,
                             ),
                           ),
-                      ),)
-
-                    ),
-                    Padding(
-                      padding: EdgeInsets.symmetric(
-                          horizontal: 27,
-                          vertical: 0),
-                      child: TextField(
-                        decoration: InputDecoration(
-                          contentPadding: EdgeInsets.all(0),
-                          hintText: 'Откуда',
-                          enabledBorder: UnderlineInputBorder(
-                            borderSide: BorderSide(
-                              color: textPassiveColor,
-                            ),
+                          prefixIcon: const Icon(
+                            UiIcons.vector_1,
+                            color: textPassiveColor,
+                            size: 18,
                           ),
-                          focusedBorder: UnderlineInputBorder(
-                            borderSide: BorderSide(
-                              color: textPassiveColor,
-                            ),
-                          ),
-                          prefixIcon: Icon(Icons.location_on, color: textPassiveColor,),
-
-                        ),
+                          prefixIconConstraints:
+                              BoxConstraints.tight(const Size(17, 17))),
+                    )),
+                Padding(
+                    padding: const EdgeInsets.only(right: 27, left: 27, top: 17),
+                    child: TextField(
+                      cursorColor: textPassiveColor,
+                      style: const TextStyle(
+                        color: textPassiveColor,
+                        fontSize: 14,
+                        fontFamily: 'Montserrat',
+                        fontWeight: FontWeight.w600,
                       ),
-
-                    ),
-                    Padding(
-                      padding: EdgeInsets.symmetric(
-                          horizontal: 27,
-                          vertical: 10),
-                      child: TextField(
-                        decoration: InputDecoration(
-                          hintText: 'Откуда',
-
-                          enabledBorder: UnderlineInputBorder(
+                      decoration: InputDecoration(
+                          isCollapsed: true,
+                          contentPadding:
+                              const EdgeInsets.symmetric(horizontal: 0, vertical: 9),
+                          hintText: 'Куда',
+                          hintStyle: const TextStyle(
+                            color: textPassiveColor,
+                            fontSize: 14,
+                            fontFamily: 'Montserrat',
+                            fontWeight: FontWeight.w600,
+                          ),
+                          enabledBorder: const UnderlineInputBorder(
                             borderSide: BorderSide(
                               color: textPassiveColor,
                             ),
                           ),
-                          focusedBorder: UnderlineInputBorder(
+                          focusedBorder: const UnderlineInputBorder(
                             borderSide: BorderSide(
                               color: textPassiveColor,
                             ),
                           ),
-                          prefixIcon: Icon(Icons.location_on, color: textPassiveColor,),
-
-                        ),
+                          prefixIcon: const Icon(
+                            UiIcons.vector_1,
+                            color: textPassiveColor,
+                            size: 18,
+                          ),
+                          prefixIconConstraints:
+                              BoxConstraints.tight(const Size(17, 17))),
+                    )),
+                Padding(
+                    padding: const EdgeInsets.only(right: 27, left: 27, top: 17),
+                    child: TextField(
+                      cursorColor: textPassiveColor,
+                      style: const TextStyle(
+                        color: textPassiveColor,
+                        fontSize: 14,
+                        fontFamily: 'Montserrat',
+                        fontWeight: FontWeight.w600,
                       ),
-
+                      decoration: InputDecoration(
+                          isCollapsed: true,
+                          contentPadding:
+                              const EdgeInsets.symmetric(horizontal: 0, vertical: 9),
+                          hintText: 'Сегодня',
+                          hintStyle: const TextStyle(
+                            color: textPassiveColor,
+                            fontSize: 14,
+                            fontFamily: 'Montserrat',
+                            fontWeight: FontWeight.w600,
+                          ),
+                          enabledBorder: const UnderlineInputBorder(
+                            borderSide: BorderSide(
+                              color: textPassiveColor,
+                            ),
+                          ),
+                          focusedBorder: const UnderlineInputBorder(
+                            borderSide: BorderSide(
+                              color: textPassiveColor,
+                            ),
+                          ),
+                          prefixIcon: const Icon(
+                            Icons.event,
+                            color: textPassiveColor,
+                            size: 18,
+                          ),
+                          prefixIconConstraints:
+                              BoxConstraints.tight(const Size(17, 17))),
+                    )),
+                SizedBox(height: 12,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: const [
+                    ServiceCard(
+                      icon: UiIcons.vector__1_,
+                      backColor: notSelectedPhoto,
+                      iconColor: primaryColor,
                     ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        ServiceCard(
-                          icon: UiIcons.vector__1_,
-                          backColor: notSelectedPhoto,
-                          iconColor: primaryColor,
-                        ),
-                        ServiceCard(
-                          icon: UiIcons.noun_delivery,
-                          backColor: backGroundColor,
-                          iconColor: textPassiveColor,
-                        ),
-                        ServiceCard(
-                          icon: UiIcons.delivery,
-                          backColor: backGroundColor,
-                          iconColor: textPassiveColor,
-                        ),
-                      ],
+                    ServiceCard(
+                      icon: UiIcons.noun_delivery,
+                      backColor: backGroundColor,
+                      iconColor: textPassiveColor,
+                    ),
+                    ServiceCard(
+                      icon: UiIcons.delivery,
+                      backColor: backGroundColor,
+                      iconColor: textPassiveColor,
                     ),
                   ],
-                ),),
-
-              Container(
-                height: 45,
-                decoration: const BoxDecoration(
-                    color: primaryColor,
-                    borderRadius:
-                    BorderRadius.vertical(bottom: Radius.circular(10))),
-                child: InkWell(
-                    onTap: () {},
-                    child: Center(
-                       child: Text('ПОИСК',
-                          style: cardText
-                      )
-                  ),
                 ),
-              )
-            ]),
+              ],
+            ),
+          ),
+          Container(
+            height: 45,
+            decoration: const BoxDecoration(
+                color: primaryColor,
+                borderRadius:
+                    BorderRadius.vertical(bottom: Radius.circular(15))),
+            child: InkWell(
+              onTap: () {},
+              child: const Center(child: Text('ПОИСК', style: cardText)),
+            ),
+          )
+        ]),
       ),
     );
   }
