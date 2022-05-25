9c202489557fa6e1efbd9b548b71282afa7f5e98
Poll21 <gol.poll21@gmail.com>
2022-05-25 10:12:28 +0700

    5455

--- lib/application/ui/constants/constants.dart
+++ lib/application/ui/constants/constants.dart
@@ -4,7 +4,12 @@ import 'package:flutter/material.dart';
 const Color primaryColor = Color(0xFF4BA35B);
 const Color backGroundColor = Colors.white;
 const Color errorColor = Color(0xFFF47458);
+
 const Color selectedColor = Color(0xFFFF9A3D);
+const Color favouriteColor = Color(0xFFFEC124);
+const Color iconDestColor = Color(0xFFFFD578);
+const Color iconDepColor = Color(0xFFEF8733);
+
 const Color textPassiveColor = Color(0xFF898989);
 const Color textActiveColor = Color(0xFF656F77);
 const Color deliveryColor = Color(0xFFC2C2C2);
