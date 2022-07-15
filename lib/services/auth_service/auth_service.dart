283aa215fec05c3e52e378351e44eb5642c2a16f
Poll <88119338+Poll21@users.noreply.github.com>
2022-07-16 06:55:10 +0700

    соединение с firebase

---
+++ lib/services/auth_service/auth_service.dart
@@ -0,0 +1,17 @@
+import 'package:firebase_auth/firebase_auth.dart';
+//
+// class AuthService {
+//   final FirebaseAuth _fAuth = FirebaseAuth.instance;
+//
+//   Future signInWithEmailAndPassword(String email, String password) async {
+//     try{
+//       // AuthResult result = await _fAuth.signInWithEmailAndPassword(email: email, password: password);
+//       // FirebaseUser user = result.user;
+//       return user;
+//     }catch(e){
+//       return null;
+//
+//     }
+//   }
+// }
+
